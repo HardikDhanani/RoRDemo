a488a30cc0fa30b4e53d4fbe216991db4afbb337
Hardik Dhanani <dhananihardik.nl@gmail.com>
2018-10-30 13:16:32 +0530

    updated navbar and user registration and edit

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -6,4 +6,14 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get pages_contact_url
+    assert_response :success
+  end
+
 end
