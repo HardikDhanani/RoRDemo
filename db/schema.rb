0ea06fea7026401c208d892d4a11c860accc44dd
Hardik Dhanani <dhananihardik.nl@gmail.com>
2018-10-30 20:11:54 +0530

    active recored associatete

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_085258) do
+ActiveRecord::Schema.define(version: 2018_10_30_142436) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
